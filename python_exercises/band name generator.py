335cabd7bfd1fb8444fe9d236c0993a681a47e16
Nate Estes <97855282+estesnr@users.noreply.github.com>
2022-06-29 09:35:21 -0400

    Add files via upload

---
+++ python_exercises/band name generator.py
@@ -0,0 +1,8 @@
+print('Welcome to band name generator\n')
+
+city = input('What city did you grow up in?\n)
+
+pet = input('What is the name of your pet?\n)
+
+print('Your band name is ' + city + ' ' + pet)
+
